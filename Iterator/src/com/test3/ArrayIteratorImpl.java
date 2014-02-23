8bf5a121001ed1c51693fcd355256f8a235804a1
zhangxv <raingxm@163.com>
2014-02-23 19:01:39 +0800

    14.3.3带迭代策略的迭代器，代码实现

--- Iterator/src/com/test3/ArrayIteratorImpl.java
+++ Iterator/src/com/test3/ArrayIteratorImpl.java
@@ -1,17 +1,16 @@
 package com.test3;
-import java.util.Iterator;
-
+import java.util.*;
 
 /**
- * 用来实现访问数组的迭代接口
+ * 用来实现访问数组的迭代接口,加入了迭代策略
  * @author zhang
  *
  */
 public class ArrayIteratorImpl implements Iterator {
 	/**
-	 * 用来存放被迭代的聚合对象
+	 * 用来存放被迭代的数组
 	 */
-	private SalaryManager aggregate = null;
+	private PayModel[] pms = null;
 	
 	/**
 	 * 用来记录当前迭代到的位置索引
@@ -19,12 +18,27 @@ public class ArrayIteratorImpl implements Iterator {
 	private int index = 0;
 	
 	public ArrayIteratorImpl(SalaryManager aggregate){
-		this.aggregate = aggregate;
+		//在这里先对聚合对象的数据进行过滤，比如工资必须在3000以下
+		Collection<PayModel> tempCol = new ArrayList<PayModel>();
+		for(PayModel pm: aggregate.getPays()){
+			//对整条数据进行过滤
+			if(pm.getPay() < 3000){
+				tempCol.add(pm);
+			}
+		}
+		
+		//然后把符合要求的数据存放在用来迭代的数组
+		this.pms = new PayModel[tempCol.size()];
+		int i = 0;
+		for(PayModel pm: tempCol){
+			this.pms[i] = pm;
+			i++;
+		}
 	}
 	
 	public boolean hasNext() {
 		//判断是否还有下一个元素
-		if(aggregate != null && index < aggregate.size()){
+		if(pms != null && index < pms.length){
 			return true;
 		}
 		return false;
@@ -34,10 +48,13 @@ public class ArrayIteratorImpl implements Iterator {
 	public Object next() {
 		Object retObj = null;
 		if(hasNext()){
-			retObj = aggregate.get(index);
+			retObj = pms[index];
 			//每取走一个值，就把已访问索引加1
 			index++;
 		}
+		//在这里对要返回的数据进行过滤，比如不让查看工资数据
+		((PayModel)retObj).setPay(0.0);
+		
 		return retObj;
 	}
 
