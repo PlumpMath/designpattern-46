67b0ec074439ba904f4b8866a2ed82060488d2bc
zhangxv <raingxm@163.com>
2014-02-26 09:45:33 +0800

    4.3.7 翻页迭代  2.随机翻页迭代的示例

---
+++ Iterator/src/com/test6/AggregationIterator.java
@@ -0,0 +1,30 @@
+package com.test6;
+import java.util.*;
+/**
+ * 定义随机翻页访问聚合元素的迭代接口
+ * @author zhang
+ *
+ */
+public interface AggregationIterator {
+	/**
+	 * 判断是否还有下一个元素，无所谓是否够一页的数据
+	 * 因为最后哪怕只有一条数据，也是要算一页的
+	 * @return	如果有下一个元素，则返回true，没有下一个元素就返回false
+	 */
+	public boolean hasNext();
+	
+	/**
+	 * 判断是否还有上一个元素，无所谓是否够一页的数据
+	 * 因为最后哪怕只有一条数据，也是要算一页的
+	 * @return	如果有上一个元素，则返回true，没有下一个元素就返回false
+	 */
+	public boolean hasPrevious();
+	
+	/**
+	 * 取出指定页数的数据
+	 * @param pageNum  需要获取的页数
+	 * @param pageShow 每页显示的数据条数
+	 * @return	指定页数的数据
+	 */
+	public Collection getPage(int pageNum,int pageShow);
+}
