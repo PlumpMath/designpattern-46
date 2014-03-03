85bb1cd699c14ff109ac605e245698d7d322666b
zhangxv <raingxm@163.com>
2014-03-03 09:42:14 +0800

    16.2.3   模板方法模式示例代码

---
+++ templatemethod/src/com/test3/ConcreteClass.java
@@ -0,0 +1,16 @@
+package com.test3;
+/**
+ * 具体实现类，实现原语操作
+ * @author zhang
+ *
+ */
+public class ConcreteClass extends AbstractClass{
+	
+	public void doPrimitiveOperation1() {
+		//具体的实现
+	}
+
+	public void doPrimitiveOperation2() {
+		//具体的实现
+	}
+}
