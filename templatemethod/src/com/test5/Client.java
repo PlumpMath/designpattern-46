850805982883677851c3cc67db00a8316dd20377
zhangxv <raingxm@163.com>
2014-03-03 11:59:33 +0800

    16.3.1  认识模板方法  5.扩展登录控制的代码

---
+++ templatemethod/src/com/test5/Client.java
@@ -0,0 +1,21 @@
+package com.test5;
+
+public class Client {
+
+	public static void main(String[] args) {
+		//准备登录人的信息
+		NormalLoginModel nlm = new NormalLoginModel();
+		nlm.setLoginId("testUser");
+		nlm.setPwd("testpwd");
+		nlm.setQuestion("testQuestion");
+		nlm.setAnswer("testAnswer");
+		
+		//准备用来进行判断的对象
+		LoginTemplate lt3 = new NormalLogin2();
+		
+		//进行登录测试
+		boolean flag3 = lt3.login(nlm);
+		System.out.println("可以进行普通人员加强版登录="+flag3);
+	}
+
+}
