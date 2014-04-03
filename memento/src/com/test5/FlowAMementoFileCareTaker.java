03f5289d47062e1049fb7b7f787f59e30a7935ba
raingxm <raingxm@163.com>
2014-04-03 09:59:08 +0800

    19.3.3   离线存储

---
+++ memento/src/com/test5/FlowAMementoFileCareTaker.java
@@ -0,0 +1,54 @@
+package com.test5;
+import java.io.*;
+/**
+ * 负责在文件中保存模拟运行流程A的对象的备忘录对象
+ * @author zhang
+ *
+ */
+public class FlowAMementoFileCareTaker {
+	/**
+	 * 保存备忘录对象
+	 * @param memento	被保存的备忘录对象
+	 */
+	public void saveMemento(FlowAMockMemento memento){
+		//写到文件中
+		ObjectOutputStream out = null;
+		try {
+			out = new ObjectOutputStream(new BufferedOutputStream(
+					new FileOutputStream("FlowAMemento")));
+			out.writeObject(memento);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally{
+			try {
+				out.close();
+			} catch (Exception err) {
+				err.printStackTrace();
+			}
+		}
+	}
+	
+	/**
+	 * 获取被保存的备忘录对象
+	 * @return	被保存的备忘录对象
+	 */
+	public FlowAMockMemento retriveMemento(){
+		FlowAMockMemento memento = null;
+		//从文件中获取备忘录数据
+		ObjectInputStream in = null;
+		try {
+			in = new ObjectInputStream(new BufferedInputStream(new FileInputStream("FlowAMemento")));
+			memento = (FlowAMockMemento)in.readObject();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally{
+			try {
+				in.close();
+			} catch (Exception err) {
+				err.printStackTrace();
+			}
+		}
+		
+		return memento;
+	}
+}
---
+++ memento/src/com/test5/FlowAMockMemento.java
@@ -0,0 +1,12 @@
+package com.test5;
+
+import java.io.*;
+
+/**
+ * 模拟运行流程A的对象的备忘录接口，是个窄接口
+ * @author zhang
+ *
+ */
+public interface FlowAMockMemento extends Serializable{
+	//空的
+}
