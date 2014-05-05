db1d7dec2b72967236474a2f03f211a82cc20c11
raingxm <raingxm@163.com>
2014-05-05 13:06:02 +0800

    21.3.3  解析器

--- Interpreter/src/com/test4/Client.java
+++ Interpreter/src/com/test4/Client.java
@@ -5,24 +5,22 @@ public class Client {
 	public static void main(String[] args) throws Exception{
 		//准备上下文
 		Context c = new Context("InterpreterTest.xml");
-		//要想获取多个d元素的值，也就是如下表达式的值："root/a/b/d$"
-		//首先要构建解释器的抽象语法树
-		ElementExpression root = new ElementExpression("root");
-		ElementExpression aEle = new ElementExpression("a");
-		ElementExpression bEle = new ElementExpression("b");
-		ElementsTerminalExpression dEle = new ElementsTerminalExpression("d");
-		
-		//组合起来
-		root.addEle(aEle);
-		aEle.addEle(bEle);
-		bEle.addEle(dEle);
-		
-		//调用
-		String ss[] = root.interpret(c);
+		//通过解析器获取抽象语法树
+		ReadXmlExpression re = Parser.parse("root/a/b/d$.id$");		
+		//请求解析，获取返回值
+		String ss[] = re.interpret(c);
 		for(String s: ss){
-			System.out.println("d的值是="+s);
+			System.out.println("d的属性id值是="+s);
 		}
 		
+		//如果要使用同一上下文，连续进行解析，需要重新初始化上下文对象
+		c.reInit();
+		ReadXmlExpression re2 = Parser.parse("root/a/b/d$");
+		//请求解析，获取返回值
+		String ss2[] = re2.interpret(c);
+		for(String s: ss2){
+			System.out.println("d的值是="+s);
+		}
 	}
 
 }
