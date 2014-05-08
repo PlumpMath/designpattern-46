d5821f681f67569f0092fb9f6fcef3fc35bb0520
raingxm <raingxm@163.com>
2014-05-08 20:07:12 +0800

    22.2.4  使用装饰模式重写示例

---
+++ Decorator/src/com/test3/Decorator.java
@@ -0,0 +1,22 @@
+package com.test3;
+/**
+ * 装饰器的接口，需要和被装饰的对象实现同样的接口
+ */
+import java.util.Date;
+
+public abstract class Decorator extends Component {
+	/**
+	 * 持有被装饰的组件对象
+	 */
+	protected Component c;
+	
+	public Decorator(Component c){
+		this.c = c;
+	}
+	
+	public double calcPrize(String user, Date begin, Date end) {
+		//转调组件对象的方法
+		return c.calcPrize(user, begin, end);
+	}
+
+}
