38b9055483badf301f312779db5ba96770ef26d7
raingxm <raingxm@163.com>
2014-05-08 19:14:38 +0800

    22.2.3  装饰模式示例代码

---
+++ Decorator/src/com/test2/Component.java
@@ -0,0 +1,12 @@
+package com.test2;
+/**
+ * 组件对象的接口，可以给这些对象动态地添加职责
+ * @author zhang
+ *
+ */
+public abstract class Component {
+	/**
+	 * 示例方法
+	 */
+	public abstract void operation();
+}
---
+++ Decorator/src/com/test2/ConcreteComponet.java
@@ -0,0 +1,12 @@
+package com.test2;
+/**
+ * 具体实现组件对象接口的对象
+ * @author zhang
+ *
+ */
+public class ConcreteComponet extends Component {
+	public void operation() {
+		//相应的功能处理
+	}
+
+}
