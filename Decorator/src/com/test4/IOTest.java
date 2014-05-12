70e530a59d730104adfd258f9d3a8906752eb5e7
raingxm <raingxm@163.com>
2014-05-12 09:48:22 +0800

    22.3.2 Java中的装饰器模式应用   I/O流

---
+++ Decorator/src/com/test4/IOTest.java
@@ -0,0 +1,25 @@
+package com.test4;
+
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class IOTest {
+	public static void main(String[] args) throws Exception{
+		//流式读取文件
+		DataInputStream din = null;
+		try {
+			din = new DataInputStream(new BufferedInputStream(new 
+					FileInputStream("IOTest.txt")));
+			//然后就可以获取文件内容
+			byte bs[] = new byte[din.available()];
+			din.read(bs);
+			String content = new String(bs);
+			System.out.println("文件内容===="+content);
+		}finally{
+			din.close();
+		}
+	}
+
+}
