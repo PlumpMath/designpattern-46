3b34451a162d18a4723f16bc4c98a62ecc8323ed
raingxm <raingxm@163.com>
2014-05-23 10:56:55 +0800

    24.2.3  桥接模式示例代码

---
+++ Bridge/src/com/test2/ConcreteImplementorB.java
@@ -0,0 +1,12 @@
+package com.test2;
+/**
+ * 真正的具体实现对象
+ * @author zhang
+ *
+ */
+public class ConcreteImplementorB implements Implementor {
+	public void operationImpl() {
+		//真正的实现
+	}
+
+}
---
+++ Bridge/src/com/test2/RefinedAbstraction.java
@@ -0,0 +1,21 @@
+package com.test2;
+/**
+ * 扩充由Abstraction定义的接口功能
+ * @author zhang
+ *
+ */
+public class RefinedAbstraction extends Abstraction {
+
+	public RefinedAbstraction(Implementor impl) {
+		super(impl);
+	}
+	
+	/**
+	 * 示例操作，实现一定的功能
+	 */
+	public void otherOperation(){
+		//实现一定的功能，可能会使用具体实现部分的实现方法
+		//但是本方法更大的可能是使用Abstraction中定义的方法
+		//通过组合使用Abstractiion中定义的方法来完成更多的功能
+	}
+}
