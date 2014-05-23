d8039c527e366e10989013c722450df118c48e94
raingxm <raingxm@163.com>
2014-05-23 11:57:19 +0800

    24.2.4  使用桥接模式重写示例

---
+++ Bridge/src/com/test3/MessageEmail.java
@@ -0,0 +1,13 @@
+package com.test3;
+/**
+ * 以E-mail的方式发送消息
+ * @author zhang
+ *
+ */
+public class MessageEmail implements MessageImplementor {
+	public void send(String message, String toUser) {
+		System.out.println("使用E-mail的方式，发送消息'"
+							+message+"'给"+toUser);
+	}
+
+}
---
+++ Bridge/src/com/test3/MessageMobile.java
@@ -0,0 +1,12 @@
+package com.test3;
+/**
+ * 以手机短消息的方式发送信息
+ * @author zhang
+ *
+ */
+public class MessageMobile implements MessageImplementor {
+	public void send(String message, String toUser) {
+		System.out.println("使用手机短消息的方式，发送信息'"+message+"'给"+toUser);
+	}
+
+}
