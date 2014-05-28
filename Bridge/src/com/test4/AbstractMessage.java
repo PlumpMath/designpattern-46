0d73d14759cb3d54a4e6c9e91d1c01277c0e5063
raingxm <raingxm@163.com>
2014-05-28 09:45:17 +0800

    (2) 不需要从外部传入参数

--- Bridge/src/com/test4/AbstractMessage.java
+++ Bridge/src/com/test4/AbstractMessage.java
@@ -6,22 +6,10 @@ package com.test4;
  */
 public abstract class AbstractMessage {
 	/**
-	 * 持有一个实现部分的对象
+	 * 构造方法
 	 */
-	protected MessageImplementor impl;
-	
-	/**
-	 * 构造方法，传入选择实现部分的类型
-	 * @param type 传入选择实现部分的类型
-	 */
-	public AbstractMessage(int type){
-		if(type == 1){
-			this.impl = new MessageSMS();
-		}else if(type == 2){
-			this.impl = new MessageEmail();
-		}else if(type == 3){
-			this.impl = new MessageMobile();
-		}
+	public AbstractMessage(){
+		//现在什么都不做了
 	}
 	
 	/**
@@ -30,6 +18,29 @@ public abstract class AbstractMessage {
 	 * @param toUser	消息发送的目的人员
 	 */
 	public void sendMessage(String message,String toUser){
-		this.impl.send(message, toUser);
+		this.getImpl(message).send(message, toUser);
+	}
+	
+	/**
+	 * 根据消息的长度选择合适的实现
+	 * @param message	要发送的消息
+	 * @return	合适的实现对象
+	 */
+	protected MessageImplementor getImpl(String message){
+		MessageImplementor impl = null;
+		if(message == null){
+			//如果没有消息，默认使用站内短消息
+			impl = new MessageSMS();
+		}else if(message.length() < 100){
+			//如果消息长度在100以内，使用手机短消息
+			impl = new MessageMobile();
+		}else if(message.length() < 1000){
+			//如果消息长度在100~1000以内，使用站内消息
+			impl = new MessageSMS();
+		}else{
+			//如果消息长度在1000以上
+			impl = new MessageEmail();
+		}
+		return impl;
 	}
 }
